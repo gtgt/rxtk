93aabf5f054d540ee5a2123e8cf1c247cc231de6
BigglesZX <biggleszx@biggleszx.com>
2010-09-08 08:17:04 +0100

    Working on horrible text/regex switching code - cleanup desperately needed

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
 		<meta http-equiv="imagetoolbar" content="no" /> 
 		
 		<link rel='stylesheet' href='style.css' type='text/css' media='screen' />
+		
+		<script src='jquery-1.4.2.min.js' type='text/javascript'></script>
+		
+		<script src='js.js' type='text/javascript'></script>
 	</head>
 	<body>
 
@@ -27,8 +31,16 @@
 			<?php
 			
 				if ($_SERVER['REQUEST_METHOD'] == "POST") {
-					$strSampleText = stripslashes($_POST['sample-text']);
-					$strRegex = stripslashes($_POST['regex']);
+					$strSampleText1 = stripslashes($_POST['sample-text-1']);
+					$strSampleText2 = stripslashes($_POST['sample-text-2']);
+					$strSampleText3 = stripslashes($_POST['sample-text-3']);
+					$strSampleText4 = stripslashes($_POST['sample-text-4']);
+					$strRegex1 = stripslashes($_POST['regex-1']);
+					$strRegex2 = stripslashes($_POST['regex-2']);
+					$strRegex3 = stripslashes($_POST['regex-3']);
+					$strRegex4 = stripslashes($_POST['regex-4']);
+					$intSampleActive = intval($_POST['sample-active']);
+					$intRegexActive = intval($_POST['regex-active']);
 					$strDelimiter = stripslashes($_POST['delimiter']);
 					$blnModI = isset($_POST['mod-i']);
 					$blnModM = isset($_POST['mod-m']);
@@ -49,7 +61,7 @@
 					if ($blnModU) { $strModifiers .= 'U'; }
 					if ($blnModU2) { $strModifiers .= 'u'; }
 					
-					$strResult = preg_replace($strDelimiter.$strRegex.$strDelimiter.$strModifiers, "<span class='match'>$0</span>", $strSampleText);
+					$strResult = preg_replace($strDelimiter.${'strRegex'.$intRegexActive}.$strDelimiter.$strModifiers, "<span class='match'>$0</span>", ${'strSampleText'.$intSampleActive});
 				}
 				else {
 					// default options
@@ -58,10 +70,12 @@
 					$blnModS = true;
 					
 					if (isset($_GET['demo'])) {
-						$strSampleText = "Enter your sample text here.\nThis is the text to which the regex will be applied.\nEnter your regex below.\nIf your regex text contains the delimiter character, you may wish to change the delimiter.\nHit the button, and see the results!";
-						$strRegex = "(^|\W)text(\W|$)";
+						$strSampleText1 = "Enter your sample text here.\nThis is the text to which the regex will be applied.\nYou can use the number selectors above to switch between different sample texts.\nEnter your regex below.\nIf your regex text contains the delimiter character, you may wish to change the delimiter.\nHit the button, and see the results!";
+						$intSampleActive = 1;
+						$intRegexActive = 1;
+						$strRegex1 = "(^|\W)text(\W|$)";
 						$strDelimiter = "/";						
-						$strResult = preg_replace($strDelimiter.$strRegex.$strDelimiter.'ims', "<span class='match'>$0</span>", $strSampleText);
+						$strResult = preg_replace($strDelimiter.$strRegex1.$strDelimiter.'ims', "<span class='match'>$0</span>", $strSampleText1);
 					}
 				}
 			
@@ -70,17 +84,47 @@
 			<div id='input-container'>
 				<form action='<?php echo basename($_SERVER['SCRIPT_NAME']); ?>' method='post'>
 					<div id='sample-input' class='input-section'>
-						<h3>
+						<h3 class='inline'>
 							Sample Text
 						</h3>
-						<textarea name='sample-text' class='textbox' rows='10'><?php if (isset($strSampleText)) { echo $strSampleText; } ?></textarea>
+						<label for='sample-switcher-1'>
+							<input type='radio' name='sample-active' value='1' id='sample-switcher-1' class='sample-switcher' <?php if (!isset($intSampleActive) || (isset($intSampleActive) && $intSampleActive == 1)) { echo "checked='checked' "; } ?>/>1
+						</label>
+						<label for='sample-switcher-2'>
+							<input type='radio' name='sample-active' value='2' id='sample-switcher-2' class='sample-switcher' <?php if (!isset($intSampleActive) || (isset($intSampleActive) && $intSampleActive == 2)) { echo "checked='checked' "; } ?>/>2
+						</label>
+						<label for='sample-switcher-3'>
+							<input type='radio' name='sample-active' value='3' id='sample-switcher-3' class='sample-switcher' <?php if (!isset($intSampleActive) || (isset($intSampleActive) && $intSampleActive == 3)) { echo "checked='checked' "; } ?>/>3
+						</label>
+						<label for='sample-switcher-4'>
+							<input type='radio' name='sample-active' value='4' id='sample-switcher-4' class='sample-switcher' <?php if (!isset($intSampleActive) || (isset($intSampleActive) && $intSampleActive == 4)) { echo "checked='checked' "; } ?>/>4
+						</label>
+						<textarea name='sample-text-1' id='sample-text-1' class='textbox sample-textbox' rows='10'><?php if (isset($strSampleText1)) { echo $strSampleText1; } ?></textarea>
+						<textarea name='sample-text-2' id='sample-text-2' class='textbox sample-textbox' rows='10' style='display: none;'><?php if (isset($strSampleText2)) { echo $strSampleText2; } ?></textarea>
+						<textarea name='sample-text-3' id='sample-text-3' class='textbox sample-textbox' rows='10' style='display: none;'><?php if (isset($strSampleText3)) { echo $strSampleText3; } ?></textarea>
+						<textarea name='sample-text-4' id='sample-text-4' class='textbox sample-textbox' rows='10' style='display: none;'><?php if (isset($strSampleText4)) { echo $strSampleText4; } ?></textarea>
 					</div>
 					
 					<div id='regex-input' class='input-section'>
-						<h3>
+						<h3 class='inline'>
 							<a href='http://www.php.net/manual/en/book.pcre.php'>PCRE Regular Expression</a>
 						</h3>
-						<textarea name='regex' rows='3' class='textbox'><?php if (isset($strRegex)) { echo $strRegex; } ?></textarea>
+						<label for='regex-switcher-1'>
+							<input type='radio' name='regex-active' value='1' id='regex-switcher-1' class='regex-switcher' <?php if (!isset($intRegexActive) || (isset($intRegexActive) && $intRegexActive == 1)) { echo "checked='checked' "; } ?>/>1
+						</label>
+						<label for='regex-switcher-2'>
+							<input type='radio' name='regex-active' value='2' id='regex-switcher-2' class='regex-switcher' <?php if (!isset($intRegexActive) || (isset($intRegexActive) && $intRegexActive == 2)) { echo "checked='checked' "; } ?>/>2
+						</label>
+						<label for='regex-switcher-3'>
+							<input type='radio' name='regex-active' value='3' id='regex-switcher-3' class='regex-switcher' <?php if (!isset($intRegexActive) || (isset($intRegexActive) && $intRegexActive == 3)) { echo "checked='checked' "; } ?>/>3
+						</label>
+						<label for='regex-switcher-4'>
+							<input type='radio' name='regex-active' value='4' id='regex-switcher-4' class='regex-switcher' <?php if (!isset($intRegexActive) || (isset($intRegexActive) && $intRegexActive == 4)) { echo "checked='checked' "; } ?>/>4
+						</label>
+						<textarea name='regex-1' id='regex-1' rows='3' class='textbox regex-textbox'><?php if (isset($strRegex1)) { echo $strRegex1; } ?></textarea>
+						<textarea name='regex-2' id='regex-2' rows='3' class='textbox regex-textbox' style='display: none;'><?php if (isset($strRegex2)) { echo $strRegex2; } ?></textarea>
+						<textarea name='regex-3' id='regex-3' rows='3' class='textbox regex-textbox' style='display: none;'><?php if (isset($strRegex3)) { echo $strRegex3; } ?></textarea>
+						<textarea name='regex-4' id='regex-4' rows='3' class='textbox regex-textbox' style='display: none;'><?php if (isset($strRegex4)) { echo $strRegex4; } ?></textarea>
 						<h3 class='inline'><a href='http://www.php.net/manual/en/regexp.reference.delimiters.php'>Delimiter</a>:</h3>&nbsp;
 						<input type='text' name='delimiter' class='textbox' size='1' maxlength='1' value='<?php if (isset($strDelimiter)) { echo $strDelimiter; } else { echo '/'; } ?>' />
 						<input type='submit' name='submit' value=' Test ' class='button' />
