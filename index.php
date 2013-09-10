6a8c077be1ab6da1c5cc67752e5da975ebdc8e82
GT <abcd@kani.hu>
2013-09-10 11:14:28 +0200

    - temp. fixed backslash in expression (double backslash is required now). should work on it later - swapped regexp / modifiers input with sample text - css: removed many borders, added some float clearing, removed some fix width. - modifiers now posted as array, form is now dynamically generated - added match dump to the bottom (Result Match) - some other fixes / improvements

--- index.php
+++ index.php
@@ -12,77 +12,353 @@
 						
 		<meta http-equiv="imagetoolbar" content="no" /> 
 		
-		<link rel='stylesheet' href='style.css' type='text/css' media='screen' />
+		<style type="text/css">
+			/*
+			 * rxtk styles
+			 *
+			 * based on "reset soup"
+			 * http://gist.github.com/549156
+			 *
+			 */
+
+
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+			 * Eric Meyer's "Resetting Again"
+			 * http://meyerweb.com/eric/thoughts/2008/01/15/resetting-again/
+			 * The big daddy of resets. I use this as a base.
+			 */
+
+			html, body, div, span, applet, object, iframe,
+			h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+			a, abbr, acronym, address, big, cite, code,
+			del, dfn, em, font, img, ins, kbd, q, s, samp,
+			small, strike, strong, sub, sup, tt, var,
+			b, u, i, center,
+			dl, dt, dd, ol, ul, li,
+			fieldset, form, label, legend,
+			table, caption, tbody, tfoot, thead, tr, th, td {
+				margin: 0;
+				padding: 0;
+				border: 0;
+				outline: 0;
+				font-size: 100%;
+				vertical-align: baseline;
+				background: transparent;
+			}
+			body {
+				line-height: 1;
+			}
+			ol, ul {
+				list-style: none;
+			}
+			blockquote, q {
+				quotes: none;
+			}
+
+			/* remember to define focus styles! */
+			:focus {
+				outline: 0;
+			}
+
+			/* remember to highlight inserts somehow! */
+			ins {
+				text-decoration: none;
+			}
+			del {
+				text-decoration: line-through;
+			}
+
+			/* tables still need 'cellspacing="0"' in the markup */
+			table {
+				border-collapse: collapse;
+				border-spacing: 0;
+			}
+
+
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+			 * Fix IE's ghastly image scaling
+			 * http://www.joelonsoftware.com/items/2008/12/22.html (and a million others)
+			 */
+
+			img {
+				-ms-interpolation-mode:bicubic;
+			}
+
+
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+			 * Fix WebKit's "background bleed"
+			 * http://tumble.sneak.co.nz/post/928998513/fixing-the-background-bleed
+			 */
+
+			.textbox, .button, .input-section {
+				-webkit-background-clip: padding-box;
+			}
+
+
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+			 * Clearfix
+			 * http://css-tricks.com/snippets/css/clear-fix/
+			 */
+
+			.clearfix:after {
+				visibility: hidden;
+				display: block;
+				font-size: 0;
+				content: " ";
+				clear: both;
+				height: 0;
+			}
+			.clearfix { display: inline-block; }
+			/* start commented backslash hack \*/
+			* html .clearfix { height: 1%; }
+			.clearfix { display: block; }
+			/* close commented backslash hack */
+
+
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+			 * Ensure "pointer" cursor on clickable elements
+			 * http://css-tricks.com/snippets/css/give-clickable-elements-a-pointer-cursor/
+			 */
+
+			a[href], input[type='submit'], input[type='image'], label[for], select, button, .pointer {
+				cursor: pointer;
+			}
+
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+			 * Prevent <sup> and <sub> from affecting line-height
+			 * http://css-tricks.com/snippets/css/prevent-superscripts-and-subscripts-from-affecting-line-height/
+			 */
+
+			sup, sub {
+				vertical-align: baseline;
+				position: relative;
+				top: -0.4em;
+			}
+			sub { top: 0.4em; }
+
+			/*
+			 * Main styles
+			 */
+
+			body {
+				font-family: Verdana, sans-serif;
+				color: #555;
+				font-size: 9pt;
+			}
+
+			h1 {
+				font-family: Georgia, serif;
+				font-size: 24pt;
+			}
+
+			h3 {
+				font-weight: bold;
+				font-size: 12pt;
+			}
+
+			h3 {
+				font-weight: bold;
+				font-size: 9pt;
+			}
+
+			a, a:visited, a:link {
+				color: inherit;
+				text-decoration: none;
+				border-bottom: 1px dotted #ccc;
+			}
+
+			a:hover {
+				border-bottom: 0;
+			}
+
+			#rap {
+				width: 700px;
+				margin: 10px auto 10px auto;
+				min-height: 400px;
+			}
+
+			#input-container {
+				width: 700px;
+				float: left;
+				margin-top: 20px;
+				border-bottom: 1px #eee solid;
+			}
+
+			.input-section, .output-section {
+				float: left;
+				padding: 10px 0;
+				margin: 2px;
+				min-height: 110px;
+				-webkit-border-radius: 3px;
+				-moz-border-radius: 3px;
+				border-radius: 3px;
+			}
+
+			.input-section h3, .output-section h3 {
+				margin-bottom: 10px;
+			}
+
+			#regex-input {
+				width: 59%;
+				border-right: 1px #eee solid;
+			}
+
+			#regex-input textarea {
+				width: 95%;
+				margin-bottom: 10px;
+			}
+
+			#regex-input h3.inline {
+				display: inline;
+			}
+
+			#regex-input input.textbox {
+				display: inline;
+				text-align: center;
+			}
+
+			#regex-input input.button {
+				float: right;
+				margin-right: 2%;
+			}
+
+			#options-input {
+				width: 39%;
+				clear: right;
+			}
+
+			#options-input .option-col {
+				width: 120px;
+				float: left;
+				font-size: 7pt;
+				margin-right: 5px;
+			}
+
+			#options-input .option-col-last {
+				margin-right: 0;
+			}
+
+			#options-input .option-col label {
+				display: block;
+			}
+
+
+			#sample-input textarea {
+				width: 98%;
+			}
+
+			.textbox, .button {
+				-webkit-border-radius: 3px;
+				-moz-border-radius: 3px;
+				border-radius: 3px;
+				border: 1px #eee solid;
+				padding: 5px;
+				background-color: #f5f5f5;
+			}
+
+			.button {
+				font-weight: bold;
+				background-color: #efefef;
+				border: 2px #ddd solid;
+			}
+
+			#result-output, #result-match, #sample-input {
+				clear: both;
+				width: 100%;
+			}
+
+			#result-output-inner {
+				-webkit-border-radius: 3px;
+				-moz-border-radius: 3px;
+				border-radius: 3px;
+				border: 1px #eee solid;
+				padding: 5px;
+				background-color: #fefefe;
+				width: 98%;
+				min-height: 110px;
+				line-height: 120%;
+			}
+			
+			#result-match-inner code > span > span:first-child {
+				display: none;
+			}
+
+			span.match {
+				background-color: #f5e881;
+			}
+
+			#error-output {
+				clear: both;
+				width: 100%;
+				min-height: 50px;
+			}
+
+			#error-output-inner {
+				-webkit-border-radius: 3px;
+				-moz-border-radius: 3px;
+				border-radius: 3px;
+				border: 1px #f00 solid;
+				padding: 5px;
+				background-color: #fefefe;
+				width: 98%;
+				min-height: 50px;
+			}
+
+			#footer {
+				text-align: center;
+				color: #aaa;
+				font-size: 7pt;
+				padding: 5px 0 5px 0;
+				clear: both;
+			}
+	</style>
 	</head>
 	<body>
 
 		<div id='rap'>
-			<h1>
-				rxtk
-			</h1>
+			<h1>rxtk</h1>
+			<h2>php regular expression toolkit</h2>
 			
 			<br />
 			<a href='<?php echo basename($_SERVER['SCRIPT_NAME']); ?>?demo'>Demo</a>
 			
 			<?php
-			
+				function escape($str) {
+					return str_replace('\\', '\\\\', $str);
+				}
+				$aMods = array('i' => 'case-insensitive', 'm' => 'multiline', 's' => 'dot matches all', 'x' => 'extended mode', 'A' => 'anchored', 'D' => 'dollar end-only', 'U' => 'ungreedy', 'u' => 'utf-8 mode');
+				$i = 0;
 				if ($_SERVER['REQUEST_METHOD'] == "POST") {
 					$strSampleText = stripslashes($_POST['sample-text']);
 					$strRegex = stripslashes($_POST['regex']);
 					$strDelimiter = stripslashes($_POST['delimiter']);
-					$blnModI = isset($_POST['mod-i']);
-					$blnModM = isset($_POST['mod-m']);
-					$blnModS = isset($_POST['mod-s']);
-					$blnModX = isset($_POST['mod-x']);
-					$blnModA = isset($_POST['mod-a']);
-					$blnModD = isset($_POST['mod-d']);
-					$blnModU = isset($_POST['mod-u']);
-					$blnModU2 = isset($_POST['mod-u2']);
-					
-					$strModifiers = '';
-					if ($blnModI) { $strModifiers .= 'i'; }
-					if ($blnModM) { $strModifiers .= 'm'; }
-					if ($blnModS) { $strModifiers .= 's'; }
-					if ($blnModX) { $strModifiers .= 's'; }
-					if ($blnModA) { $strModifiers .= 'A'; }
-					if ($blnModD) { $strModifiers .= 'D'; }
-					if ($blnModU) { $strModifiers .= 'U'; }
-					if ($blnModU2) { $strModifiers .= 'u'; }
-					
-					$strResult = preg_replace($strDelimiter.$strRegex.$strDelimiter.$strModifiers, "<span class='match'>$0</span>", $strSampleText);
+					$aModifiers = $_POST['mod'];
 				}
 				else {
 					// default options
-					$blnModI = true;
-					$blnModM = true;
-					$blnModS = true;
-					
 					if (isset($_GET['demo'])) {
 						$strSampleText = "Enter your sample text here.\nThis is the text to which the regex will be applied.\nEnter your regex below.\nIf your regex text contains the delimiter character, you may wish to change the delimiter.\nHit the button, and see the results!";
 						$strRegex = "(^|\W)text(\W|$)";
-						$strDelimiter = "/";						
-						$strResult = preg_replace($strDelimiter.$strRegex.$strDelimiter.'ims', "<span class='match'>$0</span>", $strSampleText);
+						$strDelimiter = "/";
 					}
+					$aModifiers = array("i", "m", "s");
 				}
-			
+				if (isset($strRegex)) {
+					$strResult = preg_replace($strDelimiter.$strRegex.$strDelimiter.implode("", $aModifiers), "<span class='match'>$0</span>", $strSampleText);
+					preg_match_all($strDelimiter.$strRegex.$strDelimiter.implode("", $aModifiers), $strSampleText, $aResult, PREG_SET_ORDER);
+				}
 			?>
 			
 			<div id='input-container'>
 				<form action='<?php echo basename($_SERVER['SCRIPT_NAME']); ?>' method='post'>
-					<div id='sample-input' class='input-section'>
-						<h3>
-							Sample Text
-						</h3>
-						<textarea name='sample-text' class='textbox' rows='10'><?php if (isset($strSampleText)) { echo $strSampleText; } ?></textarea>
-					</div>
 					
 					<div id='regex-input' class='input-section'>
 						<h3>
 							<a href='http://www.php.net/manual/en/book.pcre.php'>PCRE Regular Expression</a>
 						</h3>
-						<textarea name='regex' rows='3' class='textbox'><?php if (isset($strRegex)) { echo $strRegex; } ?></textarea>
+						<textarea name='regex' rows='3' class='textbox'><?php if (isset($strRegex)) { echo escape($strRegex); } ?></textarea>
 						<h3 class='inline'><a href='http://www.php.net/manual/en/regexp.reference.delimiters.php'>Delimiter</a>:</h3>&nbsp;
-						<input type='text' name='delimiter' class='textbox' size='1' maxlength='1' value='<?php if (isset($strDelimiter)) { echo $strDelimiter; } else { echo '/'; } ?>' />
+						<input type='text' name='delimiter' class='textbox' size='1' maxlength='1' value='<?php if (isset($strDelimiter)) { echo escape($strDelimiter); } else { echo '/'; } ?>' />
 						<input type='submit' name='submit' value=' Test ' class='button' />
 					</div>
 					
@@ -90,43 +366,28 @@
 						<h3>
 							<a href='http://www.php.net/manual/en/reference.pcre.pattern.modifiers.php'>Modifiers</a>
 						</h3>
-						<div class='option-col'>
-							<label for='check-mod-i'>
-								<input type='checkbox' name='mod-i' id='check-mod-i' <?php if (@$blnModI) { echo "checked='checked' "; } ?>/>
-								i (case-insensitive)
+						<?php foreach ($aMods as $sMod => $sModDescr): ?>
+						
+						<?php if ($i % 4 == 0): ?><div class='option-col'><?php endif; ?>
+
+							<label for='check-mod-<?php echo $sMod; ?>'>
+								<input type='checkbox' name='mod[]' value='<?php echo $sMod; ?>' id='check-mod-<?php echo $sMod; ?>' <?php if (array_search($sMod, $aModifiers) !== false) { echo "checked='checked' "; } ?>/>
+								<?php echo $sMod; ?> (<?php echo $sModDescr; ?>)
 							</label>
-							<label for='check-mod-m'>
-								<input type='checkbox' name='mod-m' id='check-mod-m' <?php if (@$blnModM) { echo "checked='checked' "; } ?>/>
-								m (multiline)
-							</label>
-							<label for='check-mod-s'>
-								<input type='checkbox' name='mod-s' id='check-mod-s' <?php if (@$blnModS) { echo "checked='checked' "; } ?>/>
-								s (dot matches all)
-							</label>
-							<label for='check-mod-x'>
-								<input type='checkbox' name='mod-x' id='check-mod-x' <?php if (@$blnModX) { echo "checked='checked' "; } ?>/>
-								x (extended mode)
-							</label>
-						</div>
-						<div class='option-col option-col-last'>
-							<label for='check-mod-a'>
-								<input type='checkbox' name='mod-a' id='check-mod-a' <?php if (@$blnModA) { echo "checked='checked' "; } ?>/>
-								A (anchored)
-							</label>
-							<label for='check-mod-d'>
-								<input type='checkbox' name='mod-d' id='check-mod-d' <?php if (@$blnModD) { echo "checked='checked' "; } ?>/>
-								D (dollar end-only)
-							</label>
-							<label for='check-mod-u'>
-								<input type='checkbox' name='mod-u' id='check-mod-u' <?php if (@$blnModU) { echo "checked='checked' "; } ?>/>
-								U (ungreedy)
-							</label>
-							<label for='check-mod-u2'>
-								<input type='checkbox' name='mod-u2' id='check-mod-u2' <?php if (@$blnModU2) { echo "checked='checked' "; } ?>/>
-								u (utf-8 mode)
-							</label>
-						</div>
+
+						<?php if (++$i % 4 == 0): ?></div><?php endif; ?>
+						
+						<?php endforeach; ?>
+						
 					</div>
+
+					<div id='sample-input' class='input-section'>
+						<h3>
+							Sample Text
+						</h3>
+						<textarea name='sample-text' class='textbox' rows='10'><?php if (isset($strSampleText)) { echo escape($strSampleText); } ?></textarea>
+					</div>
+
 				</form>
 			</div>
 			
@@ -134,8 +395,16 @@
 				<h3>
 					Result Text
 				</h3>
-				<div id='result-output-inner'>
+				<div id='result-output-inner' contenteditable>
 					<?php if (isset($strResult)) { echo nl2br($strResult); } ?>
+				</div>
+			</div>
+			<div id='result-match' class='output-section'>
+				<h3>
+					Result Match
+				</h3>
+				<div id='result-match-inner'>
+					<?php if (isset($aResult)) { highlight_string('<?='.var_export($aResult, 1)); } ?>
 				</div>
 			</div>
 			<!--
@@ -151,7 +420,7 @@
 			
 			<div id='footer'>
 				<br /><br />
-				<a href='http://github.com/BigglesZX/rxtk'>rxtk</a> by <a href='http://github.com/BigglesZX/'>BigglesZX</a> | <a href='http://github.com/BigglesZX/rxtk'>fork me</a> on <a href='http://github.com/'>github!</a>
+				<a href='http://github.com/BigglesZX/rxtk'>rxtk</a> by <a href='http://github.com/BigglesZX/'>BigglesZX</a> <a href='https://github.com/gtgt/rxtk'>GT</a> | <a href='http://github.com/BigglesZX/rxtk'>fork me</a> on <a href='http://github.com/'>github!</a>
 				<br /><br />
 			</div>
 		</div>
